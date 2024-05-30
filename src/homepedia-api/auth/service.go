42f27f0614b0e4c005ebe1cb7eab6ba3e3dac2f2
Antoine Romand <antoine.romand@epitech.eu>
2024-05-30 16:59:57 +0200

    Add gorm dependency, postgres driver and use singleton like

--- src/homepedia-api/auth/service.go
+++ src/homepedia-api/auth/service.go
@@ -1,7 +1,9 @@
 package auth
 
 import (
+	"fmt"
 	"homepedia-api/auth/internal/router"
+	"homepedia-api/lib/config"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,4 +11,6 @@ import (
 func InitService(echoInstance *echo.Echo) {
 	authRouter := router.NewAuthRouter(echoInstance)
 	authRouter.Register()
+	db := config.Connections.Auth
+	fmt.Println(db)
 }
--- src/homepedia-api/cmd/homepedia-api/main.go
+++ src/homepedia-api/cmd/homepedia-api/main.go
@@ -1,47 +1,22 @@
 package main
 
 import (
-	"fmt"
 	auth "homepedia-api/auth"
-	"time"
+	"homepedia-api/lib/config"
 
-	"github.com/go-co-op/gocron/v2"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Print("Hello, World!")
+	godotenv.Load()
 	echoInstance := echo.New()
-	cronInstance, err := gocron.NewScheduler()
-	if err != nil {
-		echoInstance.Logger.Fatal(err)
-	}
-	test, err := cronInstance.NewJob(
-		gocron.DurationJob(
-			10*time.Second,
-		),
-		gocron.NewTask(
-			func(a string, b int) {
-				// do things
-			},
-			"hello",
-			1,
-		),
-	)
-	if err != nil {
-		echoInstance.Logger.Fatal(err)
-	}
-	fmt.Println((test.ID()))
-	cronInstance.Start()
+	// Init connections
+	config.Init()
 
-	err = cronInstance.Shutdown()
-
-	if err != nil {
-		echoInstance.Logger.Fatal(err)
-	}
-
-	// Init service
+	// Init auth service
 	auth.InitService(echoInstance)
+
 	// Start server
 	echoInstance.Logger.Fatal(echoInstance.Start(":1323"))
 }
